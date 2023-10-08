c6d922b252345b40082cf3b76c3b08fb8c557256
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2023-10-08 18:40:16 -0300

    Habilita mapeamento dos repositórios explicitamente

--- src/main/java/imd/ufrn/familyroutine/FamilyroutineApplication.java
+++ src/main/java/imd/ufrn/familyroutine/FamilyroutineApplication.java
@@ -2,8 +2,10 @@ package imd.ufrn.familyroutine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class FamilyroutineApplication {
 
 	public static void main(String[] args) {
