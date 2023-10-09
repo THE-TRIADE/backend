31e754bb1b69797176ac13d06cf1225f759ab888
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2023-10-09 20:14:00 -0300

    Coloca anotação para resolver ciclos entre beans

--- src/main/java/imd/ufrn/familyroutine/service/GuardService.java
+++ src/main/java/imd/ufrn/familyroutine/service/GuardService.java
@@ -3,6 +3,7 @@ package imd.ufrn.familyroutine.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import imd.ufrn.familyroutine.model.FamilyGroup;
@@ -26,8 +27,10 @@ public class GuardService {
   private ValidationService validationService;
   @Autowired
   private UtilsMapper utilsMapper;
+  @Lazy
   @Autowired
   private FamilyGroupService familyGroupService;
+  @Lazy
   @Autowired
   private GuardianService guardianService;
 
